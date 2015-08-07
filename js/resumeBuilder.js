50ff9ea796c92f41062d52f024401824e5ec1ec2
Tony Chen <dj440826@gmail.com>
2015-08-07 17:22:59 +0800

    After lesson 1
    Learn append/ prepend predefined HTML (replacing %data%)to the element
    in the HTML file with javascript.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var awesomeThoughts = "My name is Tony and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+
+$("#main").append(funThoughts);
+
+var formattedName = HTMLheaderName.replace("%data%", "Tony Chen");
+var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Developer");
+$("#header").prepend(formattedName).append(formattedRole);
